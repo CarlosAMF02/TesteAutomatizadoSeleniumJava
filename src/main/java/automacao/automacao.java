90cd5f417efccb0b007e009cc5031324e5f25ab6
CarlosAMF02 <c.a.mascarenhas@hotmail.com>
2022-05-21 09:41:36 -0300

    projeto inicial

---
+++ src/main/java/automacao/automacao.java
@@ -0,0 +1,31 @@
+package automacao;
+import org.openqa.selenium.By;
+import org.openqa.selenium.*;
+
+import org.openqa.selenium.chrome.*;
+
+	import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+public class automacao {
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "D:/Compliance-Trabs/automacao/src/main/java/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.com.br/");
+
+        WebElement element = driver.findElement(By.id("twotabsearchtextbox"));
+
+        element.sendKeys("Livro George Orwel");
+        
+        element.submit();
+
+        System.out.println("Page title is: " + driver.getTitle());
+
+		 driver.quit();
+    }
+}
